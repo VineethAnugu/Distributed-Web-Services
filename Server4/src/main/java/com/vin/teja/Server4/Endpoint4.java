064751e28c5aecdeb613260f0a7272f9d3d9720b
VineethAnugu <43021915+VineethAnugu@users.noreply.github.com>
2018-12-05 15:37:47 -0500

    Div and Minus

---
+++ Server4/src/main/java/com/vin/teja/Server4/Endpoint4.java
@@ -0,0 +1,40 @@
+package com.vin.teja.Server4;
+
+import com.vin.teja.service.DivRequest;
+import com.vin.teja.service.MinusRequest;
+import com.vin.teja.service.Response;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class Endpoint4 {
+
+	@PayloadRoot(namespace = "http://teja.vin.com/service",    		
+			localPart = "DivRequest")
+	@ResponsePayload
+	public Response AddRequest(@RequestPayload DivRequest divrequest) {
+		Response response = new Response();
+
+		float a = divrequest.getNum1();
+		float b = divrequest.getNum2();
+		response.setResNum(a+b);
+		response.setComment("Success");
+		return response;
+	}
+
+	@PayloadRoot(namespace = "http://teja.vin.com/service",    		
+			localPart = "MinusRequest")
+	@ResponsePayload
+	public Response MulRequest(@RequestPayload MinusRequest minusrequest) {
+		Response response = new Response();
+
+		float a = minusrequest.getNum1();
+		float b = minusrequest.getNum2();
+		response.setResNum(a*b);
+		response.setComment("Success");
+		return response;
+	}
+}
+
---
+++ Server4/src/main/java/com/vin/teja/Server4/Server4Application.java
@@ -0,0 +1,12 @@
+package com.vin.teja.Server4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Server4Application.class, args);
+	}
+}
