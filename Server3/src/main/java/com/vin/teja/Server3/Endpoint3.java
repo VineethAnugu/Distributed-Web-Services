86fab365c0c91d93993208434e0864962da0af39
VineethAnugu <43021915+VineethAnugu@users.noreply.github.com>
2018-12-05 15:15:51 -0500

    Add files via upload

---
+++ Server3/src/main/java/com/vin/teja/Server3/Endpoint3.java
@@ -0,0 +1,40 @@
+package com.vin.teja.Server3;
+
+import com.vin.teja.service.AddRequest;
+import com.vin.teja.service.MulRequest;
+import com.vin.teja.service.Response;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class Endpoint3 {
+
+	@PayloadRoot(namespace = "http://teja.vin.com/service",    		
+			localPart = "AddRequest")
+	@ResponsePayload
+	public Response AddRequest(@RequestPayload AddRequest addrequest) {
+		Response response = new Response();
+
+		float a = addrequest.getNum1();
+		float b = addrequest.getNum2();
+		response.setResNum(a+b);
+		response.setComment("Success");
+		return response;
+	}
+
+	@PayloadRoot(namespace = "http://teja.vin.com/service",    		
+			localPart = "MinusRequest")
+	@ResponsePayload
+	public Response MulRequest(@RequestPayload MulRequest mulrequest) {
+		Response response = new Response();
+
+		float a = mulrequest.getNum1();
+		float b = mulrequest.getNum2();
+		response.setResNum(a*b);
+		response.setComment("Success");
+		return response;
+	}
+}
+
