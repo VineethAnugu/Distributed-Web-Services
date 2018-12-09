2869548830ad6b91b7d5aabf97fa1370b5c91794
VineethAnugu <43021915+VineethAnugu@users.noreply.github.com>
2018-12-08 19:10:44 -0500

    Add files via upload

--- Client/src/HTTPConnection.java
+++ Client/src/HTTPConnection.java
@@ -1,75 +1,102 @@
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
-public class HTTPConnection {
+public class HTTPConnection implements Runnable {
 
-	public static void main(String[] args) throws Exception {
-		
-		HTTPConnection http = new HTTPConnection();
-		http.sendPost();
+	private String url, postData, response;
+	
+	public HTTPConnection(String url, String postData) {
+		this.response = null;
+		this.url = url!=null?url.trim():"";
+		this.postData = this.postData!=null?this.postData.trim():"";
+	}
+	
+	public String getResponse() {
+		return response;
+	}
+	
+	
+	@Override
+	public void run() {
+		try {
+			this.response = this.sendPost(this.url, this.postData);
+		}catch (MalformedURLException e) {
+			e.printStackTrace();
+		}catch(SecurityException e) {}
 	}
 
 	
-	// HTTP POST request
-		private void sendPost() throws Exception {
+	private String sendPost(String url, String postData) throws MalformedURLException {
 
-			String url = "http://localhost:8082/Server1";
-			URL obj = new URL(url);
-			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		URL obj = new URL(url);
+		HttpURLConnection con=null;
+		
+		try {
+			con = (HttpURLConnection) obj.openConnection();
 
-			//add request header
+			con.setDoOutput(true);
 			con.setRequestMethod("POST");
 			con.setRequestProperty("Accept", "*/*");
 			con.setRequestProperty("Content-Type", "text/xml");
 			
-			// Send post request
-			con.setDoOutput(true);
-			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-		/*	wr.writeBytes("<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"\r\n" + 
-					"                  xmlns:us=\"http://teja.vin.com/service\">\r\n" + 
-					"    <soapenv:Header/>\r\n" + 
-					"    <soapenv:Body>\r\n" + 
-					"        <us:MinusRequest>\r\n" + 
-					"            <us:num1>8</us:num1>\r\n" + 
-					"            <us:num2>4</us:num2>\r\n" + 
-					"        </us:MinusRequest>\r\n" + 
-					"    </soapenv:Body>\r\n" + 
-					"</soapenv:Envelope>");*/
-			wr.writeBytes("<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"\r\n" + 
-					"                  xmlns:us=\"http://teja.vin.com/service\">\r\n" + 
-					"    <soapenv:Header/>\r\n" + 
-					"    <soapenv:Body>\r\n" + 
-					"        <us:WhichRequest>\r\n" + 
-					"            <us:serviceName>MulService</us:serviceName>\r\n" + 
-					"        </us:WhichRequest>\r\n" + 
-					"    </soapenv:Body>\r\n" + 
-					"</soapenv:Envelope>");
-			
-			wr.flush();
-			wr.close();
+			try(DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+					BufferedReader in = new BufferedReader(
+							new InputStreamReader(con.getInputStream()))) {
+				
+				wr.writeBytes(postData);
+				wr.flush();
+				int responseCode = con.getResponseCode();
+				
+				System.out.println("\nSending 'POST' request to URL : " + url);
+				System.out.println("Response Code : " + responseCode);		
+				
+				if ( responseCode == HttpURLConnection.HTTP_OK ) {
+					
+					StringBuilder response = new StringBuilder("");
+					String inputLine = null;
 
-			int responseCode = con.getResponseCode();
-			System.out.println("\nSending 'POST' request to URL : " + url);
-			System.out.println("Response Code : " + responseCode);
-
-			BufferedReader in = new BufferedReader(
-			new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
+					while ((inputLine = in.readLine()) != null) {
+						response.append(inputLine);
+					}
+					
+					return response.toString().trim();
+					
+				}
+				
+			}catch (Exception e) {
+				e.printStackTrace();
 			}
-			in.close();
 			
-			//print result
-			System.out.println(response.toString());
-
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		
+		return "Request Failed";
 
 	}
+	
+	public static void main(String[] args) throws Exception {
 
+		String data = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"\r\n" + 
+				"                  xmlns:us=\"http://teja.vin.com/service\">\r\n" + 
+				"    <soapenv:Header/>\r\n" + 
+				"    <soapenv:Body>\r\n" + 
+				"        <us:WhichRequest>\r\n" + 
+				"            <us:serviceName>MulService</us:serviceName>\r\n" + 
+				"        </us:WhichRequest>\r\n" + 
+				"    </soapenv:Body>\r\n" + 
+				"</soapenv:Envelope>";
+		
+		HTTPConnection http = new HTTPConnection("http://localhost:8082", data);
+		Thread t = new Thread(http);
+		t.start();
+		t.join();
+		System.out.println(http.getResponse());
+	}
 
+}
